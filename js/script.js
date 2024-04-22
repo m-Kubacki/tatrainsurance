7d76d86499561d80f65363789c92d3b3153b03f1
m-Kubacki <116362784+m-Kubacki@users.noreply.github.com>
2024-04-22 15:54:52 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const title = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Wracaj do nas!";
+});
+window.addEventListener("focus", () => {
+    document.title = "Najlepsze polisy na zycie!";
+});
